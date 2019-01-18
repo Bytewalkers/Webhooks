7e048ac27137050d1269c1c6ef8d3a6a3907f076
PrabhanshuAttri <contact@prabhanshu.com>
2019-01-18 15:41:30 +0530

    added basic index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.json({
+  welcome: 'home',
+}));
+
+app.get('/github-pull', (req, res) => {
+  const { params, body, query } = req;
+  console.log('params:', params);
+  console.log('body:', body);
+  console.log('query:', query);
+
+  res.json({ done: 'yes' });
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
