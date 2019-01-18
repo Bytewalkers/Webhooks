1ab86cf63b6d7f92f8240f786f27e342d460e639
PrabhanshuAttri <contact@prabhanshu.com>
2019-01-18 15:54:50 +0530

    fixed body parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({
   welcome: 'home',
